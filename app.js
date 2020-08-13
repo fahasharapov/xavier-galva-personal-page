3a6d693f849839e989d56e4301bdcbe0d9f66251
Faha Sharapov <fahasharapov@gmail.com>
2020-08-12 22:43:36 -0400

    made changes to the burger

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ burger.addEventListener('click', () => {
 		if(link.style.animation){
 			link.style.animation = '';}
 			else {
-				link.style.animation = `navLinkFade 0.5s ease forwards  ${index / 3 + 0.5}s`;
+				link.style.animation = `navLinkFade 0.5s ease forwards  ${index / 7 + 0.5}s`;
 			}
 
 	});
